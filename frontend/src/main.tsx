66fbb0dea10bc328f82fba00b35518d1b0399011
Your Name <you@example.com>
2026-06-19 19:13:12 +0200

    feat: integrate Vercel Web Analytics library and React component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
-
 import { AuthProvider } from './AuthContext.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <Analytics />
     </AuthProvider>
   </StrictMode>,
 )
